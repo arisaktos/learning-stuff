3f79613532a7d2f09b99da9c56be5033babaf246
Alicja <arisusaktos@gmail.com>
2017-12-03 18:19:31 +0100

    added browser support fallback

--- travel-site/app/assets/scripts/modules/Modal.js
+++ travel-site/app/assets/scripts/modules/Modal.js
@@ -6,7 +6,7 @@ class Modal{
         this.modal = $('.modal');
         this.closeModalButton = $('.modal__close');
         
-        this.events();
+        this.events(); 
     }
     
     events(){
