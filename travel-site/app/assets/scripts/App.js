0ecb6a4598d225118a53eeae9c451835a25a4001
Alicja <arisusaktos@gmail.com>
2017-12-04 21:30:30 +0100

    Completed gulp build task

--- travel-site/app/assets/scripts/App.js
+++ travel-site/app/assets/scripts/App.js
@@ -4,6 +4,7 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal';
 
+
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
